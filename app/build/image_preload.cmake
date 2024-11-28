7658e635e3c9d7ab55045d1f7af554945e744d7e
Naim <naeemmas@gmail.com>
2024-11-28 11:08:35 +0300

    Test new workspace format

---
+++ app/build/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "weaver_bike/nrf52833" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/opt/nordic/ncs/toolchains/15b490767d/opt/python@3.12/bin/python3.12" CACHE INTERNAL "NCS child image controlled")
